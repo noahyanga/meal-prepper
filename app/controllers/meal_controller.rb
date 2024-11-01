63beff89a273b8376523ee52fc871da02760efb8
Noah Yanga <noag29@hotmail.com>
2024-10-31 22:55:15 -0500

    Edit meal index view

--- app/controllers/meal_controller.rb
+++ app/controllers/meal_controller.rb
@@ -1,8 +1,10 @@
 class MealController < ApplicationController
   def index
-     @meals = Meal.all.limit(10) 
+    @meals = Meal.all.limit(10) 
+    @top_meals = Meal.order(created_at: :desc).limit(5)
   end
 
   def show
+    @meal = Meal.find(params[:id])
   end
 end
